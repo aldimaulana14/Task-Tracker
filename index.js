e042055dd46bbe110d95941343695da484544197
aldimaulana14 <aldimaulana14102004@gmail.com>
2026-02-27 15:10:30 +0700

    feat: tambah fitur update task

--- index.js
+++ index.js
@@ -1,48 +1,97 @@
 // Data tugas
+let currentId = 2;
 const dataTugas = [
   {
     id: 1,
-    description: "Belajar JS",
-    status: "todo",
+    description: "JS Dasar",
+    status: "In progres",
     createdAt: new Date(),
-    updatedAt: "Kosong"
-  }
-]
+    updatedAt: null,
+  },
+];
 
 // import modul readline untuk input user
 const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-
 // function tambah task
-function addTask(inputId, inputDescription, inputStatus = "todo", inputCreatedAt, inputUpdatedAt = null){
-  inputId = Date.now()
-  inputCreatedAt = new Date()
-  dataTugas.push({
-    id: inputId,
+function addTask(inputDescription, inputStatus = "todo") {
+  ``;
+  inputCreatedAt = new Date();
+
+  const newTask = {
+    id: currentId++,
     description: inputDescription,
     status: inputStatus,
     createdAt: inputCreatedAt,
-    updatedAt: inputUpdatedAt
-  })
+    updatedAt: null,
+  };
 
+  dataTugas.push(newTask);
+  console.log("Tugas berhasil ditambahkan");
+}
+
+// input tambah deskripsi
+function inputDeskripsi() {
+  rl.question("Deskripsi tugas baru: ", (desk) => {
+    if (desk.trim() === "") {
+      console.log("Deskripsi tidak boleh kosong!");
+      return inputDeskripsi(); // ulangi pertanyaan
+    }
+
+    addTask(desk, undefined);
+    showMenu();
+  });
+}
+
+function inputUpdate() {
+  // pilih data berdasarkan id
+  rl.question("Masukan id data yang mau diupdate: ", (tanyaId) => {
+    const task = dataTugas.find((item) => item.id == tanyaId);
+
+    if (!task) {
+      console.log("Data tidak ditemukan");
+      return inputUpdate();
+    }
+    // input data desk dan status baru
+    rl.question("Deskripsi Baru: ", (deskNew) => {
+      rl.question("Status Baru: ", (statusNew) => {
+        task.description = deskNew;
+        task.status = statusNew;
+        task.updatedAt = new Date();
+
+        showMenu();
+      });
+    });
+  });
+}
+
+// input perbarui task
+function UpdateTask() {
+  // tampil semua data
+  for (data of dataTugas) {
+    console.log(`===============
+      ID: ${data.id}
+      Deskripsi: ${data.description}
+      Status: ${data.status}`);
+  }
+  inputUpdate();
 }
 
 // function tampil task
-  function tampilTugas(){
-  for(data of dataTugas){
+function tampilTugas() {
+  for (data of dataTugas) {
     console.log(`===========================
 ID: ${data.id}
 Deskripsi: ${data.description}
 Status: ${data.status}
 Created At: ${data.createdAt}
-Updated At: ${data.updatedAt}`)
+Updated At: ${data.updatedAt}`);
   }
-    
 }
 
 // // tampilan awal menu
@@ -54,48 +103,36 @@ const menu = `=== Todo List App ===
 5. Daftar tugas yang sudah selesai
 6. Daftar tugas yang belum selesai
 7. Daftar tugas yang sedang dikerjakan
-8. Keluar`
+8. Keluar`;
 
 // function tampil menu
-function showMenu(){
-  console.log(menu)
+function showMenu() {
+  console.log(menu);
   rl.question("Masukan pilihan: ", (jawaban) => {
-    switch(jawaban){
+    switch (jawaban) {
       case "1":
-        rl.question("Deskripsi tugas baru: ", (desk) => {
-          addTask(undefined, desk, undefined, undefined, undefined)
-          showMenu()
-        })
-        break
+        inputDeskripsi();
+        break;
+
+      case "2":
+        UpdateTask();
+        break;
 
       case "4":
-        tampilTugas()
-        showMenu()
-        break
-      
+        tampilTugas();
+        showMenu();
+        break;
+
       case "8":
-        console.log("Terimakasih")
-        rl.close()
-        break
+        console.log("Terimakasih");
+        rl.close();
+        break;
 
       default:
-        console.log("Input tidak valid")
-        showMenu()
+        console.log("Input tidak valid");
+        showMenu();
     }
-  })
+  });
 }
 
-showMenu()
-
-
-
-
-
-
-
-
-
-
-
-
-
+showMenu();
